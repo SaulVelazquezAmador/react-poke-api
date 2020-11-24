f41be9654fe94733227cb918cc6638dab66d8f7e
Saul <sv_amador_14@outlook.com>
2020-11-23 23:00:47 -0600

    Ya carga todos los pokemon con su informacion correspondiente, corregir problema cuando se le da en el boton de siguiente por primera vez ya que la primera vez solo recarga la pagina pero no hace el cambio

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,15 +2,32 @@ import React, {useState, useEffect} from 'react'
 import Axios from 'axios'
 import Pokes from './Pokes'
 
-const Body2 = () => {
+const Body = () => {
 
     const [pokemon, setPokemon] = useState([])
+    const [page, setPage] = useState(0)
+
+    async function changeData() {
+        const res = await Axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=20`)
+        setPokemon(res.data.results)
+        console.log(res.data.results) 
+    }
+
+    const handleNext = () =>{
+        setPokemon([])
+        setPage(page + 20)
+        changeData()
+    }
+    const handlePrev = () =>{
+        setPokemon([])
+        setPage(page + 20)
+        changeData()
+    }
 
     useEffect(()=>{
         async function fetchData() {
-            const res = await Axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+            const res = await Axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
             setPokemon(res.data.results)
-            console.log(res.data.results)
         }
         fetchData();
     },[])
@@ -18,17 +35,28 @@ const Body2 = () => {
     return(
         <div className="container mx-auto">
             <div className="d-flex flex-wrap">
-                { pokemon.map((po,index)=>(
-                    <div key={index}>
-                        <Pokes 
-                            name={po.name}
-                            url={po.url}
-                        />
-                    </div>
-                ))}
+                { 
+                    pokemon.map((po,index)=>(
+                        <div key={index}>
+                            {console.log(po)}
+                            <Pokes 
+                                name={po.name}
+                                url={po.url} 
+                            />
+                        </div>
+                    ))
+                }
             </div>
+            {
+                page === 0 
+                ?   <button onClick={handleNext}>Siguiente</button>
+                :   <div>
+                        <button onClick={handlePrev}>Anterior</button> 
+                        <button onClick={handleNext}>Siguiente</button> 
+                    </div>
+            }
         </div>
     )
 }
 
-export default Body2
+export default Body
